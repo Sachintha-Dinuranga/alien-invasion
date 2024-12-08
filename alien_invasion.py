367734f5016762367df02c02f5ccbab7c34de598
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-12-08 14:10:35 +0530

    adjust ship range

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,11 +12,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     #start the main loop for the game
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
         
 run_game()
